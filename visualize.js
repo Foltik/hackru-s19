ea86e946ea2cf173a62c8977a3059e3832532876
Jack Foltz <jack@foltz.io>
2019-03-09 19:58:10 -0500

    yes

--- visualize.js
+++ visualize.js
@@ -1,49 +1,43 @@
+'use strict';
 console.log('Hello!');
 
 let group;
 d3.csv('apps.csv').then(apps => {
-    d3.csv('reviews.csv').then(reviews => {
-        console.log(apps[0]);
-        apps = crossfilter(apps);
-        reviews = crossfilter(reviews);
+    console.log(apps[0]);
+    apps = crossfilter(apps);
 
-        const ratingDim = apps.dimension(d => +d.Rating);
-        const ratingGroup = ratingDim.group();
-        const reviewDim = apps.dimension(d => +d.Reviews);
-        const reviewGroup = reviewDim.group();
-        const freeDim = apps.dimension(d => d.Type);
-        const freeGroup = freeDim.group();
+    const ratingDim = apps.dimension(d => +d.Rating);
 
-        group = freeGroup;
+    const catDim = apps.dimension(d => d.Category);
 
-        const ratingchart = dc.barChart('#ratingchart')
-            .width(768)
-            .height(480)
-            .x(d3.scaleLinear().domain([1, 5.1]))
-            .barPadding(15)
-            .dimension(ratingDim)
-            .group(ratingGroup)
-            .render();
+    const sizeDim = apps.dimension(d => +d.Size);
 
-        const reviewchart = dc.barChart('#reviewchart')
-            .width(768)
-            .height(480 )
-            .x(d3.scaleLinear().domain([0, 100000]))
-            .y(d3.scaleLinear().domain([0,100]))
-            .dimension(reviewDim)
-            .group(reviewGroup)
-            .render();
+    //group = catGroup;
 
-        const freechart = dc.barChart('#freechart')
-            .width(768)
-            .height(480)
-            .elasticX(true)
-            .x(d3.scaleBand())
-            .dimension(freeDim)
-            .group(freeGroup)
-            .render();
+    dc.barChart('#ratingchart')
+        .width(768)
+        .height(480)
+        .x(d3.scaleLinear().domain([1, 5.1]))
+        .elasticY(true)
+        .barPadding(15)
+        .dimension(ratingDim)
+        .group(ratingDim.group())
+        .render();
 
-        console.log(apps);
-        console.log(reviews);
-    });
+    dc.pieChart('#catchart')
+        .width(800)
+        .height(800)
+        .cap(10)
+        .dimension(catDim)
+        .group(catDim.group())
+        .render();
+
+    dc.barChart('#sizechart')
+        .width(800)
+        .height(800)
+        .x(d3.scaleLinear().domain([0, 100]))
+        .barPadding(15)
+        .dimension(sizeDim)
+        .group(sizeDim.group())
+        .render();
 });
